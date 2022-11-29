f76bd00919085c793f59e6fb6f1524caf4994bb9
Dhiraj Eadara <eadaradhiraj@gmail.com>
2022-11-29 14:19:20 +0000

    ENH: biblical verse

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, BiblicalVerse
 
 admin.site.register(Book)
+admin.site.register(BiblicalVerse)
+
--- books/models.py
+++ books/models.py
@@ -12,3 +12,21 @@ class Book(models.Model):
     #     indexes = [
     #         GinIndex(fields=["book"]),
     #     ]
+
+class BiblicalVerse(models.Model):
+    book = models.CharField(max_length=20)
+    verse_txt = models.TextField()
+    chapter = models.IntegerField()
+    verse_num = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['book', 'chapter', 'verse_num'],
+                name='unique_verse'
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.book} {self.chapter}:{self.verse_num}"
+
