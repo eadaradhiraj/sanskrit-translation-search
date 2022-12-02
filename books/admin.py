c766aef26052b557e3eb7476519aaa3df45b6348
Dhiraj Eadara <eadaradhiraj@gmail.com>
2022-12-02 13:05:55 +0000

    ENH: search panel admin

--- books/admin.py
+++ books/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Book, BiblicalVerse
 
-admin.site.register(Book)
-admin.site.register(BiblicalVerse)
+class VerseAdmin(admin.ModelAdmin):
+    search_fields = ['verse_title']
+
+class BookAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(BiblicalVerse, VerseAdmin)
+
